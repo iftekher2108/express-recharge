2706359210495cc3ed7491708188c976e3365810
iftekher2108 <parvezkhan21082002@gmail.com>
2025-04-28 15:56:00 +0600

    puppeteer added and this will work for html to pdf generate and some codebase change

--- Users/controller/UserController.js
+++ Users/controller/UserController.js
@@ -1,15 +1,18 @@
-const bcrypt = require('bcryptjs');
-const { generateToken } = require('../../utils/jwt');
-const User = require('../model/User');
+const bcrypt = require("bcryptjs");
+const { generateToken } = require("../../utils/jwt");
+const User = require("../model/User");
+const { html_to_pdf }  = require("../../utils/html_to_pdf");
+const path = require('path')
+const fs = require('fs')
 
-
-exports.register = async(req, res) => {
+// register function
+exports.register = async (req, res) => {
   const { name, email, password } = req.body;
   try {
     // Check if the user already exists
     const existingUser = await User.findOne({ where: { email } });
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: "User already exists" });
     }
 
     // Hash the password before saving to the database
@@ -26,50 +29,90 @@ exports.register = async(req, res) => {
     const token = generateToken(user);
 
     // Respond with the token and user data (you can choose what to send back)
-    res.status(201).json({ token, user: { id: user.id,name:user.name, email: user.email } });
+    res
+      .status(201)
+      .json({
+        token,
+        user: { id: user.id, name: user.name, email: user.email },
+      });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server error',error:err });
+    res.status(500).json({ message: "Server error", error: err });
   }
-}
+};
 
-
+// login function
 exports.login = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ where: { email } });
     if (!user) {
-      return res.status(400).json({ message: 'User not found' });
+      return res.status(400).json({ message: "User not found" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid password' });
+      return res.status(400).json({ message: "Invalid password" });
     }
 
     const token = generateToken(user); // Create JWT token
-    res.json({ token, user: { id: user.id,name:user.name, email: user.email } }); // Send token back to the client
+    res.json({
+      token,
+      user: { id: user.id, name: user.name, email: user.email },
+    }); // Send token back to the client
   } catch (err) {
-    res.status(500).json({ message: 'Server error' });
+    res.status(500).json({ message: "Server error" });
   }
-}
-
-exports.logout = async(req,res) => {
+};
 
 
-  res.json({msg:'logout successfully!'})
-}
+// logout function
+exports.logout = (req, res) => {
+  // On client: just delete token.
+  res.json({
+    message: "Logged out successfully. (Client should delete token)",
+  });
+};
+
+exports.dashboard = async (req, res) => {
+  res.json({ msg: "Welcome to the dashboard", user: req });
+};
 
 
-exports.dashboard = async(req,res) => {
-  res.json({msg: 'Welcome to the dashboard',user:req})
-}
+exports.pdfgenerate = async (req, res) => {
+  const html = `
+   <html>
+      <head>
+        <style>
+          body { font-family: Arial; margin: 50px; }
+          h1 { color: darkblue; }
+        </style>
+      </head>
+      <body>
+        <h1>Hello, PDF!</h1>
+        <p>This is generated from HTML!</p>
+      </body>
+    </html>
+  `;
 
-exports.post = async(req,res) => {
-  res.json({msg: 'this is post page'})
-}
+  await html_to_pdf(html, res);
+
+};
 
 
+exports.post = async (req, res) => {
+  res.json({ msg: "this is post page" });
+};
+
+
+
+
+
+
+
+
+
+// user store function
 exports.user_store = async (req, res) => {
   try {
     const user = await User.create(req.body);
@@ -82,32 +125,14 @@ exports.user_store = async (req, res) => {
 };
 
 
+// user data get function
 exports.user_get = async (req, res) => {
   const users = await User.findAll();
   res.json({ users: users });
 };
 
-exports.user_list = async(req, res) =>{
-  const users = await User.findAll();
-  res.json({users:users})
-}
 
-
-exports.one_milion = async(req, res) => {
-  for (let index = 0; index < 500000; index++) {
-    await User.create({name:req.body.name+`${index}`,password:req.body.password+`${index}`})
-  }
-  res.json({msg:'five milion row insert succesfully'})
-  
-}
-
-exports.one_hundred = async(req, res) => {
-  for (let index = 0; index < 100; index++) {
-    await User.create({name:req.body.name+`${index}`,password:req.body.password+`${index}`})
-  }
-  res.json({msg:'one hundred row insert succesfully'})
-}
-
+// user data delete by id function
 exports.user_delete = async (req, res) => {
   try {
     await User.destroy({
@@ -124,7 +149,6 @@ exports.user_delete = async (req, res) => {
     //     },
     //     force:true,
     // })
-    
   } catch (error) {
     res.status(400).json({ msg: `Something is wrong ${error}` });
   }
--- Users/routes/routes.js
+++ Users/routes/routes.js
@@ -3,18 +3,25 @@ const router = express.Router()
 const limiter = require('../../Config/rate-limiter')
 const UserController = require('../controller/UserController')
 const Auth = require('../../Middleware/Auth')
-const  groupRoutes = require('../../utils/GroupRoutes')
+const  groupRoutes = require('../../utils/group_routes')
 
-router.post('/user',UserController.user_store)
-router.get('/user',limiter,UserController.user_get)
-router.delete('/user/:id',UserController.user_delete)
-router.post('/one-milion',UserController.one_milion)
-router.post('/one-hundred',UserController.one_hundred)
-router.get('/user-list',UserController.user_list)
+
+router.get('/',(req,res) => {
+    res.json({msg:'Welcome to the API System'})
+})
 
 router.post('/login',UserController.login)
 router.post('/register',UserController.register)
 
+router.post('/user',UserController.user_store)
+router.get('/user',limiter,UserController.user_get)
+router.delete('/user/:id',UserController.user_delete)
+
+router.get('/pdfgenerate',UserController.pdfgenerate)
+
+
+
+// middleware before after piority matters
 groupRoutes(router,Auth,(admin) => {
     admin.get('/dashboard',UserController.dashboard)
     admin.get('/post',UserController.post) 
